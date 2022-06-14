f553b9c91220f4c6d2b090c98093f7a590847c0f
Francisco Calonje <calonjefran@gmail.com>
2022-06-13 21:07:05 -0300

    sda

---
+++ src/styles/ItemDetail.style.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const ItemDetailC = styled.div`
+display: flex;
+`;
+
+export const ItemDetailCenterC = styled.div`
+display: flex;
+`;
+
+export const ItemDetailLeftC = styled.div`
+display: flex;
+`;
+
+export const ItemDetailRightC = styled.div`
+display: flex;
+`;
+
+export const ItemDetailImage = styled.div`
+display: flex; 
+`;
+
+export const ItemDetailDescription = styled.div`
+display: flex;
+`;
+
+export const ItemDetailPrice = styled.div`
+display: flex;
+`;
+
+export const ItemDetailStockInfo = styled.div`
+display: flex;
+`;
